fdba6bef34cdbb4d4d5f44d0f81f13e4bced9644
abdurhmn.mhd <114473855+abdurhmnmhd@users.noreply.github.com>
2025-03-08 11:07:41 +0700

    Add files via upload

---
+++ resources/views/detailpelayanan.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Visi Misi</title>
+</head>
+
+<body>
+    <x-header></x-header>
+    <x-navbar>puskesmas</x-navbar>
+
+    <x-layout>
+        <div class="border-4 border-indigo-500">
+            <h1 class="text-xl font-bold..."> Apa Itu Stunting? </h1>
+            <p class="text-sm pt-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit nemo consequuntur a commodi fuga accusantium incidunt, natus voluptatem earum odit obcaecati, totam autem aspernatur quas quibusdam impedit ducimus nam aut?
+            Facere illo ullam nulla dicta explicabo natus tempore. Odio eaque molestiae, similique quos, deleniti impedit ab, eos amet ex sed natus sint ut veritatis ipsam beatae possimus aperiam. Officia, id!
+            Enim iure nemo cum, quo, excepturi eligendi, culpa dolorem quasi praesentium natus animi suscipit ipsam eos quaerat. Architecto explicabo maiores, expedita sit eveniet, non, dolor odit cum delectus iste officia!
+            Exercitationem perspiciatis vel nobis cumque delectus voluptatum accusantium, similique natus. Doloribus culpa esse animi repellendus sunt ratione earum eos dolorum, qui maxime totam, ipsa quia, impedit obcaecati laboriosam sit iusto!
+            Repudiandae, expedita cupiditate, aspernatur sapiente perferendis, adipisci incidunt quasi voluptatibus blanditiis nisi officia! Odit quod reiciendis error et expedita optio eaque amet repudiandae molestias eius! Ipsum perferendis eligendi ducimus magnam.
+            Tempore, hic id? Voluptatibus quisquam natus architecto praesentium odio iure, saepe earum! Molestias officia quas sint quo eos? Aut architecto deleniti neque esse. Deleniti sunt, est quia aperiam animi tempora?
+            Quas ab pariatur qui praesentium consectetur expedita laudantium, nesciunt atque sequi eveniet dolore ullam ipsa ad minima iste rerum soluta nobis, sit, eum veritatis. Labore sed repellendus iure recusandae fugiat?
+            Beatae accusantium tenetur, obcaecati, esse officiis magni ut odio saepe iste nemo reprehenderit error placeat officia facere architecto distinctio ab, quam est! Quam fugiat voluptatum commodi exercitationem quod sed quo.
+            Harum quas illum eligendi porro doloremque voluptatibus repudiandae aperiam. Esse, ipsa. Quisquam voluptas ex, temporibus officiis, pariatur earum obcaecati iure aspernatur non consectetur possimus molestias animi, officia expedita tempore aut!
+            Odio impedit voluptatibus officiis assumenda odit quas. Quo consequatur dolorem, iusto quos dicta repudiandae, fuga molestias cupiditate ea quaerat ducimus assumenda exercitationem illum reprehenderit. Consequuntur nulla dicta laboriosam fugit quaerat.
+            Neque illum dignissimos distinctio libero laborum amet temporibus harum veritatis non, accusantium sed dolorem quia dolore, unde est architecto facilis modi. Quae in excepturi cumque delectus, eaque mollitia sapiente quam!
+            Accusamus, eveniet! Illum, harum nihil! Quisquam fugit esse voluptatem itaque delectus sed dicta repudiandae possimus nihil. Quaerat reprehenderit quis, facere, magni dicta accusamus suscipit nemo harum molestiae quisquam earum autem.
+            Ullam ad deserunt qui facere soluta vero consequatur, blanditiis quisquam quod, saepe aspernatur debitis assumenda ea, ipsum asperiores quam laboriosam? Est aut ducimus rem cumque voluptate, voluptatibus quaerat repellendus similique?
+            Corporis nostrum porro alias omnis laboriosam quia, rem est veniam ratione recusandae vitae iure consequatur perspiciatis accusamus culpa vero aut ipsum officiis earum ut explicabo sed quibusdam assumenda rerum. Dolorum.
+            Ullam mollitia nihil asperiores dolorum id maiores numquam dolores placeat enim, atque ipsam fugiat at! Suscipit recusandae, tempore mollitia veritatis ipsam tenetur? Ad odit autem reprehenderit ipsam in quos illo.
+            Voluptates tempore quisquam eaque! Eaque dolore ducimus unde impedit voluptas modi minima tenetur fugit et odio dolorem, numquam, animi vitae sed? Earum harum eligendi et nemo laboriosam ut distinctio? Amet!
+            Mollitia laudantium, eos modi voluptatem est, ratione culpa doloribus recusandae praesentium nihil ad et accusamus nulla, eveniet ab debitis. Tempora doloribus ea natus molestias possimus, voluptate odio in. Cumque, reprehenderit?
+            Dolorem doloremque ex alias, tempora earum quibusdam fuga nobis minus adipisci labore porro in blanditiis officiis sit facilis cupiditate ducimus a assumenda nihil aspernatur? Similique eaque numquam iure aperiam repellat!
+            Dolor atque est eius tenetur, repudiandae exercitationem quibusdam numquam enim nesciunt iste tempore minus quis facilis nemo ipsa neque provident consectetur rerum! Magnam voluptas ex doloremque sapiente neque aperiam non!
+            Expedita aut amet delectus quas debitis cumque, aspernatur consequuntur dolorum nisi qui unde quam, accusamus iste? Vitae, molestias quibusdam velit, laudantium commodi nesciunt rerum optio veniam, nisi eum in. Quis.</p>
+
+        </div>
+    </x-layout>
+    <x-footer></x-footer>
+
+</body>
---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Visi Misi</title>
+</head>
+
+<body>
+    <x-header></x-header>
+    <x-navbar>puskesmas</x-navbar>
+
+    <x-layout>
+        <div class="">
+            <div>
+                <img src="" alt="">
+            </div>
+            <div>
+                <h1></h1>
+                <h3></h3>
+            </div>
+        </div>
+    </x-layout>
+
+</body>
+
+</html>
